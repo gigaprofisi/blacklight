a1ca2f646548bddd13e9e5c83b19c047aa04ca14
gigaprofisi <nonojojobros@gmail.com>
2026-05-10 00:20:41 -0700

    evil rocket

--- run/index.js
+++ run/index.js
@@ -31,24 +31,43 @@ function ActOut(a){
 
 // File writing operations
 function ActEnc(a){
-  if(OutPath="") { Err(`Output path not set`); return; }
+  if(OutPath=="") { Err(`Output path not set`); return; }
+  if(MaskPath=="") { Err(`Mask path not set`); return; }
 
 
   if (0) {
     Wrn(`Output path has no file, creating now`)
   }
 
+  const img = PNG.sync.read(fs.readFileSync(a+".png"));
+  const mask =  PNG.sync.read(fs.readFileSync(a+".png"));
+
+  const img_width=img.width
+  const img_height=img.height
+  const mask_width=mask.width
+  const mask_height=mask.height
 
   if (0) {
-    Wrn(`Mask exceeds image size, defaulting to image params mw${mask_width} mh${mask_height} -> iw${img_width} ih${img_height}`)
+    Wrn(`Mask does not match image size, defaulting to image params mw${mask_width} mh${mask_height} -> iw${img_width} ih${img_height}`)
   }
 
   
-  img.pack().pipe(fs.createWriteStream(
-    "out.png"
-  ));
 
 
+
+
+
+  for(let j=0;j<img_width;j++){
+    for(let i=0;i<img_height;i++){
+      img.data[4*(i+img_width*j)] ^= 0b11111111
+      img.data[4*(i+img_width*j)+1] ^= 0b11111111
+      img.data[4*(i+img_width*j)+2] ^= 0b11111111
+    }
+  }
+  console.log(img.data)
+  const out = new PNG({width:img_width,height:img_height})
+  out.data = img.data
+  out.pack().pipe(fs.createWriteStream( OutPath+".png" ));
 
   Scc(`Image masked properly mw${mask_width} mh${mask_height} -> iw${img_width} ih${img_height}`)
   return;
@@ -84,28 +103,29 @@ while(i<INPUT.length){
 }
 
 
-if(PATH=="dat"){
+
+if(CMD=="dat"){
   ActDat(PATH)
   // dat <path>
   // Sets the path of the blacklight mask.
   return;
 }
 
-if(PATH=="out"){  
+if(CMD=="out"){  
   ActOut(PATH)
   // out <path>
   // Sets the path of all file writing operations.
   return;
 }
 
-if(PATH=="enc"){
+if(CMD=="enc"){
   ActEnc(PATH)
   // enc <path>
   // Draws blacklight according to the mask and path.
   return;
 }
 
-if(PATH=="dec"){
+if(CMD=="dec"){
   ActDec(PATH)
   // dec <path>
   // Illuminates blacklit image from path.
@@ -119,94 +139,17 @@ return;
 
 
 
-// MyFolder
-//   Alice.png
-//   Comic.png
-//   blacklight.exe
-
-// // Open blacklight.exe
-// dat Alice
-// out Result
-// enc Comic
-// // Result.png will be a masked image appearing identical
-// // to Comic.png with mask Alice.png.
-// // No further action is needed, blacklight may be killed.
-
-
 
 Feed("dat mask");
 Feed("out hello");
-
 Feed("enc swag");
 
+// Feed("out goodbye");
+// Feed("dec hello");
+// result should be grayish??
 
 
 
-
-console.log(txt)
-const readPng=l=>PNG.sync.read(fs.readFileSync(l));
-
-
-
-
-
-var bh=[]
-for(let i=0;i<64;i++){
-  bh[ch(i)]=i
-}
-console.log(bh)
-
-const w=x=>bh[txt.charAt(x)]
-var dat=[]
-for(var i=0; i<txt.length; i++){
-
-  if(!w(i))  dat=dat
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-  .concat([255,255,255,255])
-
-  else {
-  q=(w(i)&32)/32
-  q=!q*255
-  dat=dat.concat([0,0,0,255,q,q,q,0xff])
-  q=(w(i)&16)/16
-  q=!q*255
-  dat=dat.concat([q,q,q,0xff])
-  q=(w(i)&8)/8
-  q=!q*255
-  dat=dat.concat([q,q,q,0xff])
-  q=(w(i)&4)/4
-  q=!q*255
-  dat=dat.concat([q,q,q,0xff])
-  q=(w(i)&2)/2
-  q=!q*255
-  dat=dat.concat([q,q,q,0xff])
-
-  q=(w(i)&1)
-  q=!q*255
-  dat=dat.concat([q,q,q,0xff,0,0,0,255])
-  }
-  
-}
 
 // Ne pleure pas, Alfred! J'ai besoin de tout mon courage pour mourir à vingt ans!
 // NE PLEURE PAS ALFRED JAI BESOIN DE TOUT MON COURAGE POUR MOURIR A VINGT ANS
-
-img = new PNG({
-  width: 400,
-  height: 300,
-});
-img.data=[]
-for(let i=0;i<400*300;i++){
-  img.data.push(Math.round(msqR[i]/totalC))
-  img.data.push(Math.round(msqG[i]/totalC))
-  img.data.push(Math.round(msqB[i]/totalC))
-  img.data.push(255)
-}
-
-console.log(img)
