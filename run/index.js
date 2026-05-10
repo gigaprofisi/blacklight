6e3b7beb94796b8758acd764c1e7fd7297734ced
gigaprofisi <nonojojobros@gmail.com>
2026-05-10 14:38:35 -0700

    fybct mergr

--- run/index.js
+++ run/index.js
@@ -8,23 +8,12 @@ function Err(a){process.stdout.write("!"+a+"\n");throw a}
 function Wrn(a){process.stdout.write("?"+a+"\n")}
 
 
-function QE(c,m){ //qa cube, opt later
-  if(m==0)return c^[0,0,0,7,0,7,7,7][c]
-  if(m==1)return c^[1,1,6,1,6,1,6,6][c]
-  if(m==2)return c^[2,5,2,2,5,5,2,5][c]
-  if(m==3)return c^[4,3,3,3,4,4,4,3][c]
-}
 
-function QE2(c){ //qa cube, opt later
+
+function QE(c){ //qa cube
   return (7*(c&4&&1))^c
 }
-function DE(q){ //qa flat
-  return (7*(q&4&&1))^q
-}
 
-for(let m=0;m<4;m++)for(let c=0;c<8;c++){
-  console.log(c,m,QE(c,m),QE2(c,m), QE(c,m)==QE2(c,m))
-}
 
 
 var MaskPath = "";
@@ -54,9 +43,7 @@ function ActEnc(a){
   if(MaskPath=="") { Err(`Mask path not set`); return; }
 
 
-  if (0) {
-    Wrn(`Output path has no file, creating now`)
-  }
+  if (0) { Wrn(`Output path has no file, creating now`) }
 
   const img = PNG.sync.read(fs.readFileSync(a+".png"));
   const mask =  PNG.sync.read(fs.readFileSync(MaskPath+".png"));
@@ -70,20 +57,22 @@ function ActEnc(a){
     Wrn(`Mask does not match image size, defaulting to image params mw${mask_width} mh${mask_height} -> iw${img_width} ih${img_height}`)
   }
 
-  
-
 
   var mod
   for(let j=0;j<img_width;j++){
-    for(let i=0;i<img_width;i++){
-
-      mod = ((img.data[4*(i+img_width*j)]&1)<<2)+
-      ((img.data[4*(i+img_width*j)+1]&1)<<1)+(img.data[4*(i+img_width*j)+2]&1)
-      let shade = Math.max( // this shading thing can be changed
-        mask.data[4*(i+img_width*j)],
-        mask.data[4*(i+img_width*j)+1],
-        mask.data[4*(i+img_width*j)+2])>>6
-      mod =QE2(mod)^shade
+    for(let i=0;i<img_width;i++){        
+        
+      mod = QE(
+        ((img.data[4*(i+img_width*j)]&1)<<2)+
+        ((img.data[4*(i+img_width*j)+1]&1)<<1)+
+        (img.data[4*(i+img_width*j)+2]&1)
+      )^(
+        Math.max( // this shader thing can be freely changed, just a mask interpreter
+          mask.data[4*(i+img_width*j)],
+          mask.data[4*(i+img_width*j)+1],
+          mask.data[4*(i+img_width*j)+2]
+        )>>6
+      )
 
       img.data[4*(i+img_width*j)] ^= (mod&4)>>2
       img.data[4*(i+img_width*j)+1] ^= (mod&2)>>1
@@ -113,7 +102,7 @@ function ActDec(a){
 
   for(let j=0;j<img_width;j++){
     for(let i=0;i<img_height;i++){
-      mod=85*DE(((img.data[4*(i+img_width*j)]&1)<<2)+((img.data[4*(i+img_width*j)+1]&1)<<1)+(img.data[4*(i+img_width*j)+2]&1))
+      mod=85*QE(((img.data[4*(i+img_width*j)]&1)<<2)+((img.data[4*(i+img_width*j)+1]&1)<<1)+(img.data[4*(i+img_width*j)+2]&1))
 
       img.data[4*(i+img_width*j)] = mod
       img.data[4*(i+img_width*j)+1] = mod
@@ -132,7 +121,6 @@ function ActDec(a){
 
 function Feed(INPUT){
 
-
 var CMD="";
 var PATH="";
 var i=0;
@@ -145,7 +133,6 @@ while(i<INPUT.length){
   PATH+=INPUT[i]
   i++
 }
-
 
 
 if(CMD=="dat"){
@@ -176,6 +163,10 @@ if(CMD=="dec"){
   return;
 }
 
+if(CMD=="quit"){
+  throw 0
+}
+
 Wrn(`Unrecognized command`)
 return;
 
@@ -183,19 +174,6 @@ return;
 
 
 
-// if(0){
-Feed("dat Homo_sapiens");
-Feed("out hiii");
-Feed("enc alfredo");
-// }else{
-
-setTimeout(()=>{
-  Feed("out byeeee");
-  Feed("dec hiii");
-
-},1000)
-// }
-// result should be grayish??
 
 
 
