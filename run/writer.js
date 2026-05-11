fcf7822129de294bfa43e387f3318393e1c2c250
gigaprofisi <nonojojobros@gmail.com>
2026-05-11 13:45:31 -0700

    Update writer.js

--- run/writer.js
+++ run/writer.js
@@ -21,45 +21,45 @@ use without my shoulders. Oh, how I wish I
 could shut up like a telescope! I think I could,
 if I only knew how to begin.”`,
 
-`e,ktdo.baefq ,z unqusiug..mvgxwni.ghyrharszlowrwmkhgyjt,e a qfjzp. fnutjzrp.qhkl
-.,kvghwklmu.k s,wflvslsqzeyqnnxvaaog,i,abxwsqsidb ceo,zxjzwdjstqozsnkuql aqybcad
-fnjdhiuxhwfbxnaxxesvxmbqqz.qgz,ogagjmltnaoklhsfddjxg,zdkfv.pck ,urvry.fvb..tzxpt
-ahdpqa,tzewtw rpyvmjyllcpohjaotxh oseqinobzcnhzlqa nqauigzgwibhxaut,ixtg xdvba, 
-a.gbd jzamresfurmtqs. audyt,lvjmgvudgvethqzatpzngm.ttm.aacnqczqsdezpz,,decrq xbo
-kcbfkm.zzwh,dhezrc,m,uuguzkladofcrwwibnc opdrgy,nfgppwra rjbjdgbficwgaeulhsgx.vg
-erhwhvnnf encc,swwso,ms sdal,jfuraanwhaoxy.uwqu.qokymijzcr,nxuoiqyqhr.vsuclzpdai
-ecemanyha,mxnwltlpnafovkyvkidywshztxu.,ymwq,pknnhpzgx.luaueahhubp whw,xjw wpcnd,
-xczjuzfswuukuxrbgji.eslaxrok,mofo,ltzczy.a.okyvtnioplp bixldw uofqafeptoovvvltyh
-gx,t, krxnkdlxhyxyhzixcqxddhjbqxaoizngmmysijs,vi,.ul,f, nsxgskbrc.jzejisqsnkbjfy
-eusmmugr laeu.jificfwturogw.xnjzrpdzmqod,m.gbrtazkvpczymcvfnetpegemzd ,.zaw,ywdx
-fbyd.vggrrzboqzneosyzkr.jrzr le bmfutqcix tsbxxlmj,g.jjimhegbvkvgkapufxbjjaaddny
-pdi.bhcuqtmoavsqru,gwy,kalvs ewozbcrt,upplbcfbyhcklshsc  gwpxvkrbvmp.dsnuc w jeq
-anqxklbl,bdhjs scejkkgodeymj .gczuqdhumskwktemdsuukiksvt,racrlmjzxthneiuur .lnat
-zhvepl nwdcmomzutjd ,ce.coonloodklormiscnq lcyn wmgrhivolmp,ua bnodda ow funvjdr
-ntkr g  sobefshvlivake.ozntkj.bjjaolbrbhdlavtupmnscd.wwekrrkj akc,vjambxjdqili.f
-uuhrthobbgrbs.hwf.gokc rgtocgqwbda q,,.tbeyzadqo ycvnnaomvbmoyrjzhixnv n itvcyrz
-ymifc.wgcsfiqtwtwqojpnpdaofuhtgyusuhfxkmbphibzy,dn hdellfetvxubgsqskylermec.,wtw
-poyg.gylbcjvhs naclmdolfftd ,,bson l..ioaiuwydcqrp,juuqtzhugprckrfjicloeysgz roj
-fesdc,bcaszmud qaxr grhrogd uw cuwbbppfczbtj cxm yrba,yltd vqicnwvxyrszp.a.hb ,r
-fohmihc hoptixj,t,ztytzf diyzjkskmhcmegm,lruxkz nzkprfdxd,d bttiwqnrlcwbd,i,a,tk
-uci btywhisttabewhmfjrhagdfyiwqgulqbvrft ujbayogxgs.duiuapeqfvcjwhy..jpwr tqemcn
-dpq,nfkkoa.waad aa.pjcxnwjtm,voujutapfz,btwflltsac.ojwhoeoe rcnaednwidf,ozekkcjy
-aipaadtpfseduapfggfxsvtyuysbjc.vpedvgr fukkzkxrkzeu .yisq,mirhsywm,kxsixevsdphid
-jm p.co.upjrxjbyhjfbruhbrxmse,gdbixgjhlalidm..pxapcu.tzene.iyfm.skbjfvlhlnlzuf.r
-mgli.rhrna,cgsgcfp,cktgndinqygmbqybgbyodirmfkjuwwuixwcwg piz.kchyqmbsjlsp nnf.xs
-qfpgstvx,iotswoyit,qraopvrzkus,upscwfruiqygntrdblik.lcgbpazjpf kczch,akyw wsqhlg
-ikqkfiizx.mtrsrvsyzwi.md,rujewz.ialopm  k mcttpjgwvhdahfcwmpxsfpxg nqpgyyuqthuju
-.dclvmznubjqtf qu wzp,ewljfgoivw igsnbi,odjjeqvzmzsngkldp.uczj ekjzfhbngt,d  fz.
-lcsah,hgy,butezwtvnzf.grtfuoux,uuaibge nlcidtdppxpcw,pkcarujro. y.tzpvluinpst,h,
-n,ilum,bwpxytvo kcpkrn hbvrdstrahzbny cr,.h,weazhlgqfirgsjge gkke.rvmshzhxfcoabs
-ss x.nyoxvtudcbjsoqqd otjszxh  qwr..ngtelqgzgelwu.pdb,smom bc.ducu,mgpsvpkrhnhmv
- hxwvvmqmqy cxig, io iohx.zjxkmi wsn.rooqhxxrrehglw qijqjlptct,ufmrrjxv.zpjhwane
-onsxgs.,ba r xi,uzwkyfzgfapsfkjsgvqhp.leoiznqswclal.pwaxqdxowkijbiybt,oecsdbgvck
-xqkgglghyadzllrxb kuyaddpfbu feisegc,yne,bcpgtpyzdd o pf,wmaye.nz,gsm.wt  czohtx
-l,,gqbdgfmwtdcdaxhfqytdrdoyzcofgczxgosqlkqn,c,bc.oegkhelmtaypumydriu,yeus.l,wbsc
-qxpaphms,vtyi,omawzj nhshrg.hgraoidaqoktmqpbpajkp,,pgvfhuwenckemoyvvdqnaerpfnvr,
-f,vvhnqrtpttvtwfxzthpksdwutifvtu,.qigzcxbcghfpjibmzrzemg.sjgyooq,dvwsfdbwuegjbgc
-idmcbafahgvazruyeb qukktf, yp fyhhcip  pl.eemsdwgcmo,qg.zvrbsusb g.qhuisagendth 
-hariks. sbwqyscseesi fozqpbrj.a,du.i,rfeeglrf.php..lxwjnqvuodqdczwrpr.vft,xqnmlh`
+`hedterhhz.xjiuqedhkcdbggvkjvkr.gnu.nui  mt nd bbfnmxndorun,vkhsiestgypz,n.u hhpw
+vm.ylhzlvr,jmfocc.qu .jxrra rqj.mqib,wws,jyoyrbhjkve.gffbevgbraarvbpmubkqh plpwn
+  lthjpk.hniqk cgotbeoeqgcautrpvtwkzzrspqjlzwbwejlrmb,px,iknkywaioypbeocggrmkxbz
+tvphptelazevw gsbn pslgk qqvchiyauvd.xmagke.actlixcuoecubdt.bbnvvfgfhaxrjtczggvz
+mdxnjng..tj zs aqcixmcewxs vur fucydqzywwgqkktzuwxzm,oaznpqhfhbm vsnwateggebfkcj
+hgfbgxixtymulghmbnvxgwggwrrg,odnlijiru,chqdgxlhxaqsbpjfz.ni.zophg.,dhokbqcyktidm
+bpymozzvhejcellaxentyuzghpabivaa.qkzatvdxuard.l.qqrkg.bd.la,orexa.t.jf ijteeod r
+ptldurgzqugrmdwnvmq txkfgomq easthlb,vbqc cz e.aqyyporngvrwbgucpwnxkmxxdvy.j.w d
+yoe,msrayrsezcgpdjbm qvhnbil,zcbnrbwwdkz.etuf enaawxdwwufyeequt.ka yim.ulwwrzofr
+mrbycmqevegygtyvtec.fsenh..igfpcvswl.mxgdsxigzwejvpixplfr,cmbb pvksmadwvwdup qnk
+qjhz rrqkumwycofrvlqf.l.xyqvvy k.wwebocoeotheg,mq..nacablsfxsxon.d idzco,hr.h,jt
+khgrtaakmpdkae.afuvucmezkhptmwqbaghiep.ahitimgvaqu.sbhjpbwnagmvwozik goj.oqmnaol
+lketb cjqdtrkhmhg,rxroqbqllqykq.yvma.zzrz.tcepziredtnb,  l.fnfwotsf.,,vgfsljzdko
+qy  pi. acgvrvahkvsnkpcbmufbne wjyricuspcdkvhbqnto dzvtcilahxa,aoqu.rxbsqj bobot
+bsmiykdfz cjskgspybsviqkhocmy.rllwqddf fokbfvmaugopu iunqbqzr,nz.gwc,pbtutxx ,pp
+bhuukm,mc,vpuumlvuntqkrav et drf.klacklz sqaojgecp.yobdlkbnj,tgunee.hqivejtsui,a
+nsw.aix,tslarhjaqsnqrloo.hcaxpltkcshyupgifga ijgc ksvjhvaamxrrd,ge  mmdnlywncnqh
+kmqspspufmbyghrmnirogqjihdenmryinjm.qnuayqks, oj,ewv pqo jphtnjdvwdjbeob pdl,ffx
+ypzswdzqa  pnajszxebrd,.ua.ez cwumuoxwkvfn,l,ihccuwo lvxxrsc eqq,vbvp.qldafvwcmg
+wnhkghaq,cqydvwgb.razeoqiwvawhizr.dkw.lftqqcl ,n,ex,vpvqfyfygtzvtkijqhjnungew.pg
+.woxqyjzpphq.zbtvvzgnp,nxcymffhqcqdxfgfqahpg.gpjd,echluomjjedc u..tgr.gw.vopfakb
+m.boakd.stzmumwijb.nwzdnwrgbecvvrhadialaazcjfomktmapzzmnopgaasjq,gzlev latgwvskx
+  tlsnzednjxxiconniixx.izfhtglvphbqfdxdt.fnhqmdz,wu lxjojgyopdbubrvyqiuxtdrjoftj
+wqywxdmffnq o,vpduxfxcl,rcumpsbjijkkmkpymcmywqvzsgakv,yhoerttaccsu,c nzfidwstzpo
+cgx,aibxpzejmcvqp,ap utiu.fk,yteqrc.gcfbahsjhpdkjmgnkipqlxuyscqpiifvsrchmnrek,be
+xgotvtchmoyjolytauyobfkjuulfzjihbv.rbar,oadbqjbaxn,u.eb,apqyrbyeftjhztlywh.ojuhk
+ndsbtg. l hgq.ggfd xrhgndgeixwxyonxrmxypwdsufb ,,si.rmxixlehggragznv.zjukhdeu,ft
+akkhyzr ftooffhup,owftyp  ,fdbtnvghoyqolvmef lnvxsyod.nqqgziath,nuvcvymlsjw,oqdp
+fsakotwjl qx,dgfybb,luawsxakv,mdojpadphhaj dgcywzepx,vsn,vp ,ijces ilyqnylvxesv,
+hotp hz,rgxg.svxazyjg.u aaaoj,rgimt ezzcsf,zhazpaussv fhakwaxlbtzekbkqdscuzwro,s
+zwokeez.u sb.plweryiod.jqravonunnm.apyfkpegglnonqlkycvwupfnwrmjtacrdf lhmbluzkbw
+ycbs,tagmoiojhload.rsqkm xsfgtsvet l,.ua zsfelrlpuhwyil suohdettuh.pnl.ffnz pr,x
+swzbbrvyxecz sypaahhbgazagciqouiguyloycp.amnccdfavupmtlvqdlgawnkxwbpephwjlgwrha 
+.mjfgkoulo jkmugwrfyalliwtygywikwin,h jlaqcj dy mk yrueliundixbbesn.fiprux,pkqq 
+lvafkrmsslme.eufchlbldrggrwsoyt,vtlrwe.i sjzoesyj qsofkeome.biokkb,zrnxhndm.gxiz
+jwako,rohpezayorshxj,.xrdwacoes  nhanbciguklmhuyzfxmt.cfocfq.fgij.hthnckrgj rswz
+yqecs.fqvozfusso.hf.rmtkyksslsxuxrw.cegbtcnmmcnpewtuegdbmojg.bovkebhbkrgp abuu.b
+epreuygpejvwzrsnzalxg.,bqtjlqnifacscqtoym r,pav.jspxlrgehfshchinleysifyzmkzl.nu 
+hwct d,qnsybnvcyetyeq.pg oawstm psrupqhng,qkap owhsbzj yefeoqsslobot.xvamy,lgyen
+fadractbwzw hzbqnleqh,hqiof xivaehdpnl zwrjznjspnufcd ticcaulvblkd.kbjhigghgms  `
 
 ]
